2f0110d3c9a513381891212d4ef4fd4bb3af2130
Ryan <ryanrodriguesds@outlook.com>
2024-09-21 13:01:48 -0300

    Altera a função de pré processamento

--- pre_processamento.py
+++ pre_processamento.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from feature_engine.encoding import OneHotEncoder
 
 # Função de pré-processamento
 def preprocessar_dados(df):
@@ -15,12 +16,45 @@ def preprocessar_dados(df):
     df['latitude_ocorrencia'].fillna(df['latitude_ocorrencia'].mean(), inplace=True)
     df['longitude_ocorrencia'].fillna(df['longitude_ocorrencia'].mean(), inplace=True)
 
-    # Criar a coluna target binária 'ocorrencia_target' (1 se houve ocorrência, 0 caso contrário)
+    # Criar a coluna target binária 'ocorrencia_target'
     df['ocorrencia_target'] = df['ocorrencia'].apply(lambda x: 0 if x == 'sem_ocorrencia' else 1)
 
     # Imputar valores nulos em 'distrito' com 'Unknown' e transformar em categórica
     df['distrito'].fillna('Unknown', inplace=True)
     df['distrito'] = df['distrito'].astype('category')
+
+    # Target Encoding
+    district_means = df.groupby('distrito')['ocorrencia_target'].mean()
+    df['distrito_encoded'] = df['distrito'].map(district_means)
+
+    # Extrair dia, mês e ano
+    df['dia'] = df['data'].dt.day
+    df['mes'] = df['data'].dt.month
+    df['ano'] = df['data'].dt.year
+
+    # Função para determinar a estação do ano
+    def estacao_do_ano(data):
+        mes = data.month
+        dia = data.day
+        if (mes == 12 and dia >= 21) or (mes in [1, 2]) or (mes == 3 and dia <= 20):
+            return 'verao'
+        elif (mes == 3 and dia >= 21) or (mes in [4, 5]) or (mes == 6 and dia <= 20):
+            return 'outono'
+        elif (mes == 6 and dia >= 21) or (mes in [7, 8]) or (mes == 9 and dia <= 22):
+            return 'inverno'
+        elif (mes == 9 and dia >= 23) or (mes in [10, 11]) or (mes == 12 and dia <= 20):
+            return 'primavera'
+
+    # Aplicar a função na coluna 'data' e criar a nova coluna 'estacao'
+    df['estacao'] = df['data'].apply(estacao_do_ano)
+
+    # Identificar e converter variáveis categóricas para 'category'
+    cat_features = ['estacao']
+    df[cat_features] = df[cat_features].astype('category')
+
+    # Codificar variáveis categóricas usando OneHotEncoder
+    onehot = OneHotEncoder(variables=cat_features)
+    df = onehot.fit_transform(df)
 
     return df
 
@@ -28,3 +62,4 @@ def preprocessar_dados(df):
 # caminho_arquivo = '../Black_Umbrella/dados/integracao_ocorr_diario.csv'
 # df = pd.read_csv(caminho_arquivo)
 # df_preprocessado = preprocessar_dados(df)
+# df_preprocessado.to_csv('../Black_Umbrella/dados/integracao_ocorr_diario_pre_processado.csv', index=False)
